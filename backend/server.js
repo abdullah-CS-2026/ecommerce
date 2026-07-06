f1ad71ef13e923f6267141ab26748cfe4addabea
Abdullah-536 <iamabdullah536@gmail.com>
2026-07-06 04:42:43 -0700

    fis: updating

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:[ "https://ecommercewebsite-zeta-nine.vercel.app",'http://localhost:5173'],  // Vite frontend URL
-  credentials: true // Allow cookies
+  origin: [
+    "http://localhost:5173",
+    "https://ecommercewebsite-zeta-nine.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
