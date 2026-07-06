4aeb4a6d564eb2b5688f0fcdb5cb99da0e499a3d
Abdullah-536 <iamabdullah536@gmail.com>
2026-07-06 04:47:55 -0700

    fis: updating deploying

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://ecommercewebsite-zeta-nine.vercel.app"
+    "https://technovawebiste.vercel.app"
   ],
   credentials: true
 }));
