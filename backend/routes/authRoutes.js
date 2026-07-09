ab0c2e562a5dc2ea58a35234dbd3efe6a7314231
Abdullah-536 <iamabdullah536@gmail.com>
2026-07-09 06:11:34 -0700

    fis: email

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/register', authController.registerUser);
+// router.post('/register', authController.registerUser);
+router.post('/register', (req, res) => {
+  console.log("REGISTER ROUTE HIT");
+
+  return res.json({
+    success: true,
+    message: "Route works"
+  });
+});
 router.post('/login', authController.loginUser);
 router.post('/verify-email', authController.verifyEmail);
 router.post('/resend-otp', authController.resendOtp);
